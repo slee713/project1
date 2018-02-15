e2d8f64697849085fdc19695bd2fd22b9edd9006
James Kang <jkang@medallia.com>
2018-02-15 11:14:23 -0500

    fixing regex in urls

--- project1/courses/urls.py
+++ project1/courses/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'$', views.course_list),
+	url(r'^$', views.course_list),
 	url(r'(?P<pk>\d+)/$', views.course_detail),
 ]
